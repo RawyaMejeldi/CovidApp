6b3de9d70fb59bf00a996913c53e8ed13700d2b0
marwenbk <marwanbk2000@gmail.com>
2020-06-27 11:09:46 +0100

    master

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,46 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import './screens/LoginCard.dart';
 import './screens/LoginScreen.dart';
-import './screens/splash_screen.dart';
+import 'package:geolocator/geolocator.dart';
 import 'screens/aboutUs.dart';
 import 'screens/home.dart';
 import 'screens/information.dart';
 import 'screens/yourLocation.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  runApp(MyApp());
+  final _auth = FirebaseAuth.instance;
+  var geolocator = Geolocator();
+  var authRes = await _auth.currentUser();
+  final prefs = await SharedPreferences.getInstance();
+  var location = json.decode(prefs.getString('location'));
+  var locationOptions =
+      LocationOptions(accuracy: LocationAccuracy.high, distanceFilter: 10);
+  geolocator
+      .getPositionStream(locationOptions)
+      .listen((Position position) async {
+    if (position != null) {
+      double distanceInMeters = await Geolocator().distanceBetween(
+          position.latitude,
+          position.longitude,
+          location['lat'],
+          location['long']);
+      if (distanceInMeters > 100) {
+        await Firestore.instance
+            .collection('User break rules')
+            .document(authRes.uid)
+            .setData({'lat': position.latitude, 'long': position.longitude});
+      }
+    }
+  });
+}
 
 class MyApp extends StatelessWidget {
   @override
@@ -24,7 +55,7 @@ class MyApp extends StatelessWidget {
         title: 'Télésuivi covid-19',
         // home: LogInCard(),
         home: SafeArea(
-                  child: StreamBuilder(
+          child: StreamBuilder(
               stream: FirebaseAuth.instance.onAuthStateChanged,
               builder: (ctx, userSnapshot) {
                 // if (userSnapshot.connectionState == ConnectionState.waiting) {
@@ -37,7 +68,7 @@ class MyApp extends StatelessWidget {
               }),
         ),
         routes: {
-          LoginScreen.pageRoute:(_)=>LoginScreen(),          
+          LoginScreen.pageRoute: (_) => LoginScreen(),
           Home.pageRoute: (_) => Home(),
           AboutUs.pageRoute: (_) => AboutUs(),
           Information.pageRoute: (_) => Information(),
